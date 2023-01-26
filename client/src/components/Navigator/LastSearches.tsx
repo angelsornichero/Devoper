bb3ffb0b2410581ab6c76e91b39e93704f310dc7
angelsornichero <angelsornicheropacheco2008@gmail.com>
2023-01-26 16:53:09 +0100

    Last searches history added

---
+++ client/src/components/Navigator/LastSearches.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const LastSearches = () => {
+  return (
+    <div>LastSearches</div>
+  )
+}
--- client/src/hooks/useVideos.tsx
+++ client/src/hooks/useVideos.tsx
@@ -17,6 +17,14 @@ const useVideos = ({ keyword }: Params) => {
     setLoading(false)
   }
 
+  const loadLastKeywords = () => {
+    if (keyword === '-') return
+    if (!localStorage.getItem('lastSearches')) return localStorage.setItem('lastSearches', JSON.stringify([keyword]))
+    const lastSearches = JSON.parse(localStorage.getItem('lastSearches') as string)
+    lastSearches.push(keyword)
+    localStorage.setItem('lastSearches', JSON.stringify(lastSearches))
+  }
+
   useEffect(() => {
     loadVideos()
   }, [])
