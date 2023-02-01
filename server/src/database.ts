55b69571dac10deb171a20cd367f480c4a7b8928
angelsornichero <angelsornicheropacheco2008@gmail.com>
2023-02-01 19:03:39 +0100

    App deploy

--- server/src/database.ts
+++ server/src/database.ts
@@ -1,10 +1,17 @@
 import console from "console";
+import { prototype } from "events";
 import mongoose from "mongoose";
 
 
 (async () => {
     try {
-        const db = await mongoose.connect(process.env.MONGO_URL_DEV as string)
+        const db = await mongoose.connect(
+            process.env.MONGO_URL_DEV as string,
+            {
+                "user": process.env.MONGO_USER,
+                "pass": process.env.MONGO_PASSWORD
+            }
+        )
         console.log('[*] Database connected to: ', db.connection.name)
     }
     
